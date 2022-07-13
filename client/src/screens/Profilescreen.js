e36956e4a22d38fcbebcb527947aa6dc7c796392
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-14 01:29:09 +0500

    Fetching booking details in profile and making a cancel booking button

--- client/src/screens/Profilescreen.js
+++ client/src/screens/Profilescreen.js
@@ -1,5 +1,7 @@
 import { Tabs } from 'antd';
 import React,{useEffect,useState} from 'react';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
 import axios from 'axios';
 const { TabPane } = Tabs;
 
@@ -16,11 +18,11 @@ function Profilescreen() {
         <div className='ml-3 mt-3'>
             <Tabs defaultActiveKey="1">
                 <TabPane tab="Profile" key="1">
-                    <h1>My Profile</h1>
+                    <p>My Profile</p>
                     <br />
-                    <h1>Name : {user.name}</h1>
-                    <h1>Email : {user.email}</h1>
-                    <h1>isAdmin : {user.isAdmin ? 'Yes' : 'No'}</h1>
+                    <p>Name : {user.name}</p>
+                    <p>Email : {user.email}</p>
+                    <p>isAdmin : {user.isAdmin ? 'Yes' : 'No'}</p>
                 </TabPane>
                 <TabPane tab="Booking" key="2">
                     <MyBookings/>
@@ -34,14 +36,22 @@ export default Profilescreen
 
 export function MyBookings(){
     const user = JSON.parse(localStorage.getItem('currentuser'));
+    const [booking , setbooking] = useState([]);
+    const [loading, setloading] = useState(false);
+    const [error, seterror] = useState();    
     useEffect( () => {
         async function fetchData() {
             
             try {
-                const rooms = await (await axios.post('/api/bookings/getbookingsbyuserid',{userid : user._id})).data;
-                console.log(rooms)
+                setloading(true);
+                const data = await (await axios.post('/api/bookings/getbookingsbyuserid',{userid : user._id})).data;
+                console.log(data);
+                setbooking(data);
+                setloading(false);
             } catch (error) {
                 console.log(error)
+                setloading(false);
+                seterror(error);
             }
         }
         fetchData();
@@ -49,7 +59,26 @@ export function MyBookings(){
     
     return(
         <div>
-            <h1>My Bookings</h1>
+            <div className="row">
+                <div className="col-md-6">
+
+                    {loading && (<Loader/>)}
+                    {booking && (booking.map(booking=>{
+                        return <div className='bs'>
+                            <p><b>{booking.room}</b></p>
+                            <p><b> Bookingid</b> : {booking._id}</p>
+                            <p><b> CheckIn</b> : {booking.fromdate}</p>
+                            <p><b> Check Out</b>: {booking.todate}</p>
+                            <p><b> Amount</b> : {booking.totalamount}</p>
+                            <p><b>Status</b> : {booking.status === 'booked' ? 'CONFIRMED' : 'CANCELLED'}</p>
+                            <div className='text-right'>
+                            <button className='btn btn-primary '>CANCEL BOOKING</button>
+
+                            </div>
+                        </div>
+                    }))}
+                </div>
+            </div>
         </div>
     )
 }
